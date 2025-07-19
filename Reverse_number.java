e0c9b338ad1dbd9096aaede33eb18c446fe41928
Vijilee George Kurian <vijileegk2006@gmail.com>
2025-07-19 11:46:02 +0530

    Add files via upload

---
+++ Reverse_number.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class Reverse_number {
+	public static void main(String[] args) {
+		Scanner number=new Scanner(System.in);
+		System.out.println("Enter a number:");
+		int num=number.nextInt();
+		int num_2=num;
+		int reversed_num=0;
+		int remainder;
+		while(num!=0)
+		{
+			remainder=num%10;
+			reversed_num=reversed_num*10+remainder;
+			num=num/10;
+		}
+		System.out.println("The reversed number is:"+reversed_num);
+		if(num_2==reversed_num) {
+			System.out.println("It is a Palindrome number");
+		}
+		else {
+			System.out.println("It Is not a Palindrome number");
+		}
+		
+	}
+
+}
